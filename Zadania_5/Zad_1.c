c5aed4e05f4cb26b670af592208ee37e4fc00cd0
GrzegorzDrozdz <147175116+GrzegorzDrozdz@users.noreply.github.com>
2024-04-14 14:13:44 +0200

    Update Zad_1.c

--- Zadania_5/Zad_1.c
+++ Zadania_5/Zad_1.c
@@ -6,16 +6,26 @@ typedef struct node {
     struct node *next;
 } Node;
 
-void add_first(Node **head, int value) {
+Node* createNode(int value) {
     Node *new_node = (Node *)malloc(sizeof(Node));
+    if (new_node == NULL) {
+        printf("Błąd alokacji pamięci\n");
+        exit(EXIT_FAILURE);
+    }
     new_node->value = value;
+    new_node->next = NULL;
+    return new_node;
+}
+
+void add_first(Node **head, int value) {
+    Node *new_node = createNode(value);
     new_node->next = *head;
     *head = new_node;
 }
 
 void print_list(Node *head) {
     Node *current = head;
-    while (current!= NULL) {
+    while (current != NULL) {
         printf("%d -> ", current->value);
         current = current->next;
     }
@@ -25,7 +35,9 @@ void print_list(Node *head) {
 int main() {
     Node *head = NULL;
     add_first(&head, 3);
+    print_list(head);
     add_first(&head, 2);
+    print_list(head);
     add_first(&head, 1);
     print_list(head);
     return 0;
