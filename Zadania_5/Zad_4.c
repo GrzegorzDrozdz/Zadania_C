62412e0f17532549b554e19c7526c7c919a85dcf
GrzegorzDrozdz <147175116+GrzegorzDrozdz@users.noreply.github.com>
2024-04-14 14:23:36 +0200

    Update Zad_4.c

--- Zadania_5/Zad_4.c
+++ Zadania_5/Zad_4.c
@@ -1,83 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct listElement {
-    int data;
-    struct listElement *next;
-} listElement;
+typedef struct node {
+    int value;
+    struct node *next;
+} Node;
 
-listElement *head = NULL;
-
-void push(int val) {
-    listElement *new_node = malloc(sizeof(listElement));
+Node* createNode(int value) {
+    Node *new_node = (Node *)malloc(sizeof(Node));
     if (new_node == NULL) {
-        fprintf(stderr, "Błąd alokacji pamięci.\n");
+        printf("Błąd alokacji pamięci\n");
         exit(EXIT_FAILURE);
     }
-
-    new_node->data = val;
-    new_node->next = head;
-    head = new_node;
+    new_node->value = value;
+    new_node->next = NULL;
+    return new_node;
 }
 
-void print_list() {
-    listElement *current = head;
-    printf("Lista: ");
+void add_first(Node **head, int value) {
+    Node *new_node = createNode(value);
+    new_node->next = *head;
+    *head = new_node;
+}
+
+void print_list(Node *head) {
+    Node *current = head;
     while (current != NULL) {
-        printf("%d -> ", current->data);
+        printf("%d -> ", current->value);
         current = current->next;
     }
     printf("NULL\n");
 }
 
-void remove_by_index(int index) {
-    if (head == NULL) {
-        printf("Lista jest pusta. Nie można usunąć elementu.\n");
+void remove_by_index(Node **head, int index) {
+    if (*head == NULL) {
+        printf("Lista jest pusta\n");
         return;
     }
-
-    if (index <= 0) {
-        listElement *temp = head;
-        head = head->next;
+    if (index == 0) {
+        Node *temp = *head;
+        *head = (*head)->next;
         free(temp);
         return;
     }
-
-    listElement *current = head;
-    listElement *prev = NULL;
+    Node *current = *head;
+    Node *prev = NULL;
     int i = 0;
-
     while (current != NULL && i < index) {
         prev = current;
         current = current->next;
         i++;
     }
-
     if (current == NULL) {
-        printf("Indeks przekracza długość listy. Nie można usunąć elementu.\n");
+        printf("Nieprawidłowy indeks\n");
         return;
     }
-
-     prev->next = current->next;
-
-     free(current);
+    prev->next = current->next;
+    free(current);
 }
 
 int main() {
+    Node *head = NULL;
+    add_first(&head, 3);
+    add_first(&head, 2);
+    add_first(&head, 1);
+    printf("Lista przed usunieciem elementu na indeksie 1:\n");
+    print_list(head);
 
-    push(3);
-    push(2);
-    push(1);
+    remove_by_index(&head, 1);
 
-    printf("Lista przed usunięciem elementu:\n");
-    print_list();
-
-
-    remove_by_index(1);
-
-
-    printf("Lista po usunięciu elementu:\n");
-    print_list();
+    printf("Lista po usunieciu elementu na indeksie 1:\n");
+    print_list(head);
 
     return 0;
 }
