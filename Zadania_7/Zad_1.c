13f5f30dc8bb2699429b2ad05ba4cda455f6706c
GrzegorzDrozdz <147175116+GrzegorzDrozdz@users.noreply.github.com>
2024-05-06 20:55:42 +0200

    Add files via upload

---
+++ Zadania_7/Zad_1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main() {
+    FILE *file;
+    char line[100];
+    int sum = 0;
+
+    file = fopen("../Zadania_7\\input.txt", "r");
+
+    if (file == NULL) {
+        printf("Nie udało się otworzyć pliku.\n");
+        return 1;
+    }
+
+    while (fgets(line, sizeof(line), file)) {
+        char *ptr = line;
+        int first_digit = -1, last_digit = -1;
+
+        while (*ptr != '\0') {
+            if (isdigit(*ptr)) {
+                if (first_digit == -1) {
+                    first_digit = *ptr - '0';
+                }
+                last_digit = *ptr - '0';
+            }
+            ptr++;
+        }
+
+        if (first_digit != -1 && last_digit != -1) {
+            int two_digit_number = first_digit * 10 + last_digit;
+            sum += two_digit_number;
+        }
+    }
+
+    fclose(file);
+
+    printf("Suma liczb dwucyfrowych: %d\n", sum);
+
+    return 0;
+}
---
+++ Zadania_7/Zad_2.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int strToNum(char *str) {
+    if (strcmp(str, "zero") == 0) return 0;
+    if (strcmp(str, "one") == 0) return 1;
+    if (strcmp(str, "two") == 0) return 2;
+    if (strcmp(str, "three") == 0) return 3;
+    if (strcmp(str, "four") == 0) return 4;
+    if (strcmp(str, "five") == 0) return 5;
+    if (strcmp(str, "six") == 0) return 6;
+    if (strcmp(str, "seven") == 0) return 7;
+    if (strcmp(str, "eight") == 0) return 8;
+    if (strcmp(str, "nine") == 0) return 9;
+    return -1;
+}
+
+int main() {
+    FILE *file;
+    char line[100];
+    int sum = 0;
+
+    file = fopen("../Zadania_7\\input.txt", "r");
+
+    if (file == NULL) {
+        printf("Nie udało się otworzyć pliku.\n");
+        return 1;
+    }
+
+    char *words[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    while (fgets(line, sizeof(line), file)) {
+        int first = -1, last = -1;
+        int len = strlen(line);
+
+        for (int i = 0; i < len; i++) {
+            if (isdigit(line[i])) {
+                first = line[i] - '0';
+                break;
+            } else {
+                for (int j = 0; j < 10; j++) {
+                    if (strncmp(&line[i], words[j], strlen(words[j])) == 0) {
+                        first = strToNum(words[j]);
+                        break;
+                    }
+                }
+                if (first != -1) break;
+            }
+        }
+
+        for (int i = len - 1; i >= 0; i--) {
+            if (isdigit(line[i])) {
+                last = line[i] - '0';
+                break;
+            } else {
+                for (int j = 0; j < 10; j++) {
+                    if (strncmp(&line[i], words[j], strlen(words[j])) == 0) {
+                        last = strToNum(words[j]);
+                        break;
+                    }
+                }
+                if (last != -1) break;
+            }
+        }
+
+        if (first != -1 && last != -1) {
+            sum += first * 10 + last;
+        }
+    }
+
+    fclose(file);
+
+    printf("Suma liczb dwucyfrowych: %d\n", sum);
+
+    return 0;
+}
